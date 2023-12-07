884c3b5a0f82b37fdc829a37a5dafdc387fa2ea1
jhon jairo garavito 2627062 <149621084+jhongaravitoADSO@users.noreply.github.com>
2023-12-06 23:04:38 -0500

    Add files via upload

---
+++ bd/formulario.php
@@ -0,0 +1,40 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recopilar datos del formulario
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    $opinion = $_POST["opinion"];
+    $calificacion = $_POST["calificacion"];
+    $fechaDeCreacion = date("Y-m-d H:i:s"); 
+    // Configuración de la conexión a la base de datos
+    $servidor = "MySQL Community Server - GPL";
+    $usuario = "root";
+    $contrasena = "root";
+    $basededatos = "opiniones"; 
+
+    // Crear conexión
+    $conexion = new mysqli($servidor, $usuario, $contrasena, $basededatos);
+
+    // Verificar la conexión
+    if ($conexion->connect_error) {
+        die("Error en la conexión a la base de datos: " . $conexion->connect_error);
+    }
+
+    // Preparar la consulta SQL para insertar los datos en la base de datos
+    $consulta = $conexion->prepare("INSERT INTO opiniones (nombre, correo, opinion, calificacion, fecha_creacion) VALUES (?, ?, ?, ?, ?)");
+    $consulta->bind_param("sssss", $nombre, $correo, $opinion, $calificacion, $fechaDeCreacion);
+
+    // Ejecutar la consulta
+    if ($consulta->execute()) {
+        echo "<p>Gracias por tu opinión, $nombre.</p>";
+        echo "<p>Correo electrónico: $correo</p>";
+        echo "<p>Opinión: $opinion</p>";
+        echo "<p>Calificación: $calificacion</p>";
+    } else {
+        echo "Error al insertar en la base de datos: " . $conexion->error;
+    }
+
+    // Cerrar la conexión
+    $conexion->close();
+}
+?>
